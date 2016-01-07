b786ca26abfe53c6ddfb02fa355ceed4bc10bdfa
ascotson <ascotson@gmail.com>
2016-01-06 19:45:50 -0700

    All tasks completed. App not working. Need to debug. Possible error in teamCtrl.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,35 @@ app.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
 
     $httpProvider.interceptors.push('httpRequestInterceptor');
 
-    // routing configuration code
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateURL: 'js/home/homeTmpl.html',
+        controller: 'homeCtrl',
+        resolve: {
+          jazzData: function($stateParams, teamService){
+            return teamService.getTeamData('utahjazz');
+          },
+          lakersData: function($stateParams, teamService){
+            return teamService.getTeamData('losangeleslakers');
+          },
+          heatData: function($stateParams, teamService){
+            return teamService.getTeamData('miamiheat');
+          }
+        }
+      })
 
+      .state('teams', {
+        url: '/teams/:team',
+        templateURL: 'js/teams/teamTmpl.html',
+        controller: 'teamCtrl',
+        resolve: {
+          teamData: function($stateParams, teamService) {
+            return teamService.getTeamData($stateParams.team);
+          }
+        }
+      });
+
+    $urlRouterProvider
+      .otherwise('/');
 });
